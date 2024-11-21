a031dad0827ac135e499225eeb396a798178800d
alanmarinic <marinic.alan@gmail.com>
2024-11-21 21:29:30 +0100

    add main controller, menu route and view

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1">
+
+        <title>Categories</title>
+    </head>
+
+    <body class="bg-gray-50">
+        hello
+
+        <script>
+
+        </script>
+    </body>
+</html>

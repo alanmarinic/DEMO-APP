c1afd0c50b95464df1d8c9fbd95688e0f24685e6
alanmarinic <marinic.alan@gmail.com>
2024-11-22 03:19:22 +0100

    add support for api routes; add api routes for root- and sub-categories

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 Route::get('/categories', [CategoryController::class, 'categories']);
+Route::get('/root-categories', [CategoryController::class, 'rootCategories']);
+Route::get('/{categoryId}/subcategories', [CategoryController::class, 'subcategories']);
